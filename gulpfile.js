e48a9b4b5ee5ef9d4084e487729924b1839af34d
lazardavidovic <44703091+lazardavidovic@users.noreply.github.com>
2018-11-07 01:17:21 +0100

    Create gulp JavaScript tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,75 @@
 var gulp = require('gulp');
+var header = require('gulp-header');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+var browserSync = require('browser-sync').create();
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function () {
 
   // Bootstrap
   gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
+    './node_modules/bootstrap/dist/**/*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  ])
     .pipe(gulp.dest('./vendor/bootstrap'))
 
   // Font Awesome
   gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
+    './node_modules/@fortawesome/**/*',
+  ])
     .pipe(gulp.dest('./vendor'))
 
   // jQuery
   gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery'))
 
   // jQuery Easing
   gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
+    './node_modules/jquery.easing/*.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery-easing'))
 
+});
+
+// Set the banner content for JavaScript and CSS
+var banner = ['/*!\n',
+  ' * Lazar Davidovic - Resume - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2018-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' */\n',
+  '\n'
+].join('');
+
+// Minify JavaScript
+gulp.task('js:minify', function () {
+  return gulp.src([
+    './js/*.js',
+    '!./js/*.min.js'
+  ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+});
+
+// AIO JavaScript task
+gulp.task('js', ['js:minify']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
