f6df16dfcfa832c7e0ab449fd58a23b91841f2fd
lazardavidovic <44703091+lazardavidovic@users.noreply.github.com>
2018-11-07 01:00:48 +0100

    Create gulp CSS tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 var pkg = require('./package.json');
 var browserSync = require('browser-sync').create();
 
@@ -44,6 +47,39 @@ var banner = ['/*!\n',
   ' */\n',
   '\n'
 ].join('');
+
+// Compile SCSS
+gulp.task('css:compile', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function () {
+  return gulp.src([
+    './css/*.css',
+    '!./css/*.min.css'
+  ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+// AIO CSS task
+gulp.task('css', ['css:compile', 'css:minify']);
 
 // Minify JavaScript
 gulp.task('js:minify', function () {
